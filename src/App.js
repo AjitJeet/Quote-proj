7862d3ab9333ee37cbc851f033c5e5c30183552b
AjitJeet <ajitkr15839@gmail.com>
2023-02-02 20:47:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  
+  const getColor = () => {
+    // Hex Code
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
+
+    navigator.clipboard.writeText(randomCode)
+}
+
+//event call
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+
+
+// init call
+getColor();
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);  
+          setAuthor(quote.author);
+        }
+      )
+  },[]);
+
+  let fetchNewQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);  
+          setAuthor(quote.author);
+        }
+      )
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <div className="quote">
+            <h2>{quote}</h2>
+            <small>-{author}-</small>
+         </div><br />
+         <button className="btn" onClick={fetchNewQuote}>New Quote</button>
     </div>
   );
 }
